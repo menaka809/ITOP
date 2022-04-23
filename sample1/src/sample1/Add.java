6b0622fc257f2ae68e455a8d55fa8b96476801be
menaka809 <malithjayasekara2255721@gmail.com>
2022-04-23 20:06:51 +0530

    initial state

---
+++ sample1/src/sample1/Add.java
@@ -0,0 +1,16 @@
+package sample1;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=8,b=7;
+		System.out.println(a+b);
+		System.out.println();
+		
+		
+		
+
+	}
+
+}
